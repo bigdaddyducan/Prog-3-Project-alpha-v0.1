2de70b49b14ca84c90a67cee182382bdfebb403a
Erin <erinmckeever7@gmail.com>
2024-11-20 22:26:22 +0000

    entered code for booking

--- Prog-3-Project-alpha-v0.1-master/Prog 3 Project alpha v0.1/Bookings.cs
+++ Prog-3-Project-alpha-v0.1-master/Prog 3 Project alpha v0.1/Bookings.cs
@@ -41,6 +41,80 @@ namespace Prog_3_Project_alpha_v0._1
 
         }
 
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (1stStaff.Items.Count! = 0)
+                    {
+                pnlBooking.Enabled = true;
+            }
+        }
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (1stTreatment.Items.Count! = 0)
+                    {
+                dsProg_3_Project_alpha_v0.Tables["Patient"].Clear();
+
+                cmdTreatmentDetails.Parameters[""].value = 1stTreatment.SelectedValue;
+
+                daStaff.Fill(dsProg_3_Project_alpha_v0, "Staff");
+
+
+                1stStaff.DataSource = Prog_3_Project_alpha_v0.Tables[""];
+                1stStaff.DisplayMember = "";
+                1stStaff.ValueMember = "";
+
+                1stStaff.SelectedIndex = -1;
+            }
+        }
+
+        private void fillListboxPatients(String str)
+        {
+            cmdPatientsDetails.Parameters["@Letter"].Value = str + "%";
+
+            daPatients.Fill(dsProg_3_Project_alpha_v0, "Patient");
+
+            1stPatient.DataSource = dsProg_3_Project_alpha_v0.Tables["Patient"];
+            1stPatient.DisplayMember = "name";
+            1stPatient.ValueMember = "PatientNo";
+        }
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String title = "";
+
+            dsProg_3_Project_alpha_v0.Tables["Patient"].Clear();
+
+            cmdPatientDetails.Parameters["@CustNo"].Value = lstPatients.SelectedValue;
+
+            daPatients.Fill(dsProg_3_Project_alpha_v0, "Patients");
+
+            1stPatients.DataSource = dsProg_3_Project_alpha_v0.Tables["Patient"];
+            1stPatient.DisplayMember = "name";
+            1stPatient.Valuemember = "breedNo";
+
+            1stPatient.SelectedIndex = -1;
+
+            drPatient = dsProg_3_Project_alpha_v0.Tables["Patient"].Rows.Find(1stPatient.SelectedValue);
+
+            if (drPatient["Title"].ToString() == "Mr")
+                title = "Mr";
+            if (drPatient["Title"].ToString() == "Mrs")
+                title = "Mrs";
+            if (drPatient["Title"].ToString() == "Miss")
+                title = "Miss";
+            if (drPatient["Title"].ToString() == "Ms")
+                title = "Ms";
+
+            lblPat0.Text = drPatient["PatientNo"].ToString();
+            lblPat1.Text = title + " " + drPatient["Forename"].ToString() + " " + drPatient["Surname"].ToString();
+            lblPat2.Text = drPatient["Street"].ToString();
+            lblPat3.Text = drPatient["Town"].ToString();
+            lblPat4.Text = drPatient["County"].ToString();
+            lblPat5.Text = drPatient["Postcode"].ToString();
+        }
+
+
         private void btnAdd_Click(object sender, EventArgs e)
         {
             DataRow drBookings, drBookingsDerts;
@@ -56,12 +130,35 @@ namespace Prog_3_Project_alpha_v0._1
                 MessageBox.Show("Please select Staff", "Staff");
             else if (lvwBooking.Items.Count == 0)
                 MessageBox.Show("Please add a patient/staff to the booking", "Booking Details");
+            else
+            {
+                drBookings = Prog_3_Project_alpha_v0.Tables["Booking"].NewRow();
+
+                drBookings["BookingNo"] = bookingNo;
+                Bookings["PatientNo"] = int.Parse(lblPat0.Text);
+                Bookings["dateBooked"] = DateTime.Parse(lblBookingDate.Text.Trim());
+                Bookings["dateStart"] = DateTime.Parse(dtpStartDate.Text.Trime());
+
+                dsProg_3_Project_alpha_v0.Tables["Booking"].Rows.Add(drBooking);
+                daBooking.Update(dsProg_3_Project_alpha_v0, "Booking");
+
+                foreach (ListViewItem item in lvwBooking.Items)
+                {
+                    drBookingsDets = dsProg_3_Project_alpha_v0.Tables["BookingDetail"].NewRow();
+                    drBookingsDets["bookingNo"] = drBookings["bookingNo"];
+                    drBookingsDets["Treatment"] = int.Parse(item.SubItems[0].Text);
+                    drBookingsDets["Staff"] = int.Parse(item.SubItems[2].Text);
+                    dsProg_3_Project_alpha_v0.Tables["BookingDetail"].Rows.Add(drBookingsDets);
+                    drBookingsDets.Update(dsProg_3_Project_alpha_v0, "BookingDetail");
+
+                }
+
+                MessageBox.Show("Booking No: " + drBookings["bookingNo"].ToString() + " added to system");
+
+                pnlBooking.Enabled = false;
+
+            }
         }
-
-
-
-
-
 
         private void AddPatient_Click(object sender, EventArgs e)
         {
@@ -82,22 +179,54 @@ namespace Prog_3_Project_alpha_v0._1
                 {
                     if (item.SubItems[1].Text == 1stPatient.Text || item.SubItems[2].Text == 1stStaff)
                     {
-                        MessageBox.Show("")
+                        MessageBox.Show("Patient or Staff already selected for this booking.", "Booking");
+                        exists = true;
+                        break;
+                    }
+                }
+
+                if (!exits)
+                {
+                    DateTime start = DateTime.Parse(dtpStartDate.Text.Trim());
+                    foreach (DataRow dr in Prog_3_Project_alpha_v0._1.Tables["BookedStaff"].Rows)
+                    {
+                        DateTime bookedDate = DateTime.Parse(dr["dateStart"].ToString());
+
+                        if (start >= bookedDate && start <= bookedDate.AddDays(int.Parse(cmbNoOfDays.Text)))
+                        {
+                            if ((dr["PatientNo"] == 1stPatient.SelectedValue) || (dr[].ToString() == 1stPatient.Text))
+                                {
+                                MessageBox.Show("Either the selected patient or staff is already included in a booking for this date range. Please re-select.", "Booking");
+                                ok = false;
                             }
+                            if (!ok)
+                                break;
+                        }
+                    }
+                    if (ok)
+                    {
+                        foreach (DataRow dr in Prog_3_Project_alpha_v0.Tables[""].Rows)
+                        {
+                            if (dr[""].ToString() == 1stPatient.Text)
+                                    {
+                                ListViewItem item = new ListViewItem(dr["patientno"].ToString());
+                                item.SubItems.Add(dr[""].ToString());
+                                item.SubItems.Add(1stTreatment.Text);
+                                lvwBooking.Items.Add(item);
+                                break;
+                            }
+                        }
+                    }
                 }
             }
-
-
 
         }
 
 
 
-
-
         private void DeletePatient_Click(object sender, EventArgs e)
         {
-            if(lvwBooking.SelectedItems.Count ! = 0)
+            if (lvwBooking.SelectedItems.Count! = 0)
             {
                 var item = lvwBooking.SelectedItems[0];
                 lvwBooking.Items.Delete(item);
@@ -116,5 +245,12 @@ namespace Prog_3_Project_alpha_v0._1
             lblPat4.Text = "";
             lblPat5.Text = "";
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
     }
-}
